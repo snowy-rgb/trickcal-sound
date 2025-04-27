1cbb4354a699e6df430081c9abd3fa301602c9b1
snowy <trigger.snowii@gmail.com>
2025-04-27 19:29:17 +0900

    Update audioPlay.js

--- sys/js/playButton/audioPlay.js
+++ sys/js/playButton/audioPlay.js
@@ -1,5 +1,5 @@
 let komiSounds = [
-    new Audio('../../../../assets/audio_but/but.m4a'), // 첫 번째 소리 파일
+    new Audio('assets/audio_but/but.m4a'), // 첫 번째 소리 파일
     new Audio('../../../../assets/audio_but/ter.m4a')  // 두 번째 소리 파일
   ];
   let komIndex = 0;
